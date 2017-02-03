536b1c1b6a1a090f48c92a5c69f3c004bc46b7d7
Silas Mächler <smaechler@Raptuss-MacBook-Pro-2.local>
2017-02-03 15:15:44 +0100

    startet with listing

---
+++ classes/components/class.peoplelisting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Forge\Modules\People\Components;
+
+use \Forge\Core\Abstracts\Component;
+use \Forge\Core\App\App;
+
+use function \Forge\Core\Classes\i;
+
+class People extends Listing {
+
+}
+
+?>
